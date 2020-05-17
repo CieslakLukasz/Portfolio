2e1a5fda4c5456fbfc92c3d408584970e8ad446f
CieslakLukasz <lukaszcieslak90@gmail.com>
2020-05-17 15:06:34 +0200

    Updates

---
+++ precache-manifest.294763f5df8875dba28619bc00e79135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4154866df7bc24c86106ba0e9ac2e710",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7295fd82dda4c14ce664",
+    "url": "/static/css/main.764a0adf.chunk.css"
+  },
+  {
+    "revision": "550ef3658b1eff12d1e2",
+    "url": "/static/js/2.fab58f87.chunk.js"
+  },
+  {
+    "revision": "7694ed5eff56d74dfc4b6ac3fc78a12b",
+    "url": "/static/js/2.fab58f87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7295fd82dda4c14ce664",
+    "url": "/static/js/main.8e5154a4.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
